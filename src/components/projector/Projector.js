97be581adf8104d978b54a634167d76091de2f84
Rob Huehn <rhuehn@gmail.com>
2017-05-09 13:57:43 -0700

    convert project to list item

--- src/components/projector/Projector.js
+++ src/components/projector/Projector.js
@@ -1,16 +1,15 @@
 import {h, Component} from 'preact'
-import {Icon, Text, Switch, focus} from '@theatersoft/components'
+import {Icon, ListItem, Switch, focus} from '@theatersoft/components'
 
 export const Projector = ({dispatchDeviceAction, value, id, onClose}) =>
     <div class="inset container">
-        <div class="col">
-            <Text text="Projector"/>
+        <Icon icon="cross" cb={onClose}/>
+        <ListItem label="Projector">
             <Switch
                 checked={value}
                 onChange={() => dispatchDeviceAction(switchAction(value, id))}
             />
-        </div>
-        <Icon icon="cross" cb={onClose}/>
+        </ListItem>
     </div>
 
 import {connect} from '../../redux'
