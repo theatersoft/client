eb47e27a9b08ba896d348bc4bc98aeca87bfe5cb
Rob Huehn <rhuehn@gmail.com>
2017-05-18 20:53:20 -0700

    rm Text from pinpad

--- src/components/pinpad/Pinpad.js
+++ src/components/pinpad/Pinpad.js
@@ -1,5 +1,5 @@
 import {h, Component} from 'preact'
-import {grid, Icon, Text} from '@theatersoft/components'
+import {grid, Icon} from '@theatersoft/components'
 import {focus, mixinFocusable} from '@theatersoft/focus'
 import rpc from '../../rpc'
 import {refresh} from '../../auth'
@@ -34,7 +34,7 @@ export default class Pinpad extends mixinFocusable(Component) {
             <div class={style.key} onClick={() => this.onkey(n)}>{n}</div>
         return <div class="container" id={style.pinpad}>
             {grid([
-                [<Text text={pin}/>],
+                [<div class={style.text}>{pin}</div>],
                 [key(1), key(2), key(3), <Icon icon="cross" cb={() => focus.pop()}/>],
                 [key(4), key(5), key(6), key(0)],
                 [key(7), key(8), key(9), <Icon icon="lock" cb={() => this.onenter()}/>]
