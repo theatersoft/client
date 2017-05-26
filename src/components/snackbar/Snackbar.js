33c6ec3aa132165717fc7b7d07188e437c61cc90
Rob Huehn <rhuehn@gmail.com>
2017-05-26 16:08:22 -0700

    rm logs

--- src/components/snackbar/Snackbar.js
+++ src/components/snackbar/Snackbar.js
@@ -2,22 +2,14 @@ import {h, Component} from 'preact'
 import {Snackbar} from '@theatersoft/components'
 import {connect} from '../../redux'
 
-const
-    mapStateToProps = ({devices}) => {
-        console.log('mapStateToProps', devices)
-        const
-            feed = devices && devices['Automation.feed'],
-            value = feed && feed.value
-        return {
-            active: value && value.active,
-            label: value && value.message
-        }
+const mapStateToProps = ({devices}) => {
+    const
+        feed = devices && devices['Automation.feed'],
+        value = feed && feed.value
+    return {
+        active: value && value.active,
+        label: value && value.message
     }
+}
 
-export default connect(mapStateToProps)
-(class extends Component {
-    render (props) {
-        console.log('Snackbar render')
-        return <Snackbar {...props}/>
-    }
-})
+export default connect(mapStateToProps)(props => <Snackbar {...props}/>)
