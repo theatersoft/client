8bb700f8ac26e705e75c44b123da985255cff905
Rob Huehn <rhuehn@gmail.com>
2017-06-04 07:57:53 -0700

    rm unused styl and ref

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,35 +1,25 @@
 import {h, Component} from 'preact'
 import {Button, TapMenu, mousePosition, touchPosition} from '@theatersoft/components'
-import {Video} from '../video'
-import {mixinFocusable} from '@theatersoft/focus'
-import style from './menu.styl'
-import {focus} from '@theatersoft/focus'
+import {Video} from '../'
+import {focus, mixinFocusable} from '@theatersoft/focus'
+import {log} from '@theatersoft/bus'
 
 export default class Menu extends mixinFocusable(Component) {
     state = {active: false}
 
-    ref = menu => {this.menu = menu}
-
     onActive = active => this.setState({active})
 
-    onGesture = e => {
-        if (!this.state.active) Video.onGesture(e)
-    }
+    onGesture = e => {if (!this.state.active) Video.onGesture(e)}
 
-    onKeydown = e => {
-        if (!this.state.active) Video.onKeydown(e)
-    }
+    onKeydown = e => {if (!this.state.active) Video.onKeydown(e)}
 
-    onMousedownCapture = e => {
-        if (Video.testEvent(e)) e.preventDefault()
-    }
+    onMousedownCapture = e => {if (Video.testEvent(e)) e.preventDefault()}
 
     render ({items, ...props}) {
         return (
             <div onMousedownCapture={this.onMousedownCapture}>
                 <TapMenu
                     actions={Object.entries(items).map(([icon, onClick]) => ({icon, onClick}))}
-                    ref={this.ref}
                     onActive={this.onActive}
                     {...props}
                 />
