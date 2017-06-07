8ebeed61655524bedda41a076f129d68b720b6a2
Rob Huehn <rhuehn@gmail.com>
2017-06-07 15:36:32 -0700

    swap spinner and logo in menu

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {Menu, Pinpad, Stat, Projector, Lights, Snackbar} from './components'
 
 const items = [
     <Menu name="menu" items={{
-                logo: () => focus.push('lights'),
                 spinner: () => window.location.reload(),
+                logo: () => focus.push('lights'),
                 thermometer: () => focus.push('stat'),
                 list: () => focus.push('projector')
             }}/>,
