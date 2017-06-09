9d14e5bd889f9e9ea90be65e89825b2f416015bb
Rob Huehn <rhuehn@gmail.com>
2017-06-09 07:30:38 -0700

    add menu enter key handling to activate tapmenu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,18 +2,26 @@ import {h, Component} from 'preact'
 import {Button, TapMenu, mousePosition, touchPosition} from '@theatersoft/components'
 import {Video} from '../'
 import {focus, mixinFocusable} from '@theatersoft/focus'
-import {log} from '@theatersoft/bus'
 
 export default class Menu extends mixinFocusable(Component) {
     state = {active: false}
 
     onActive = active => this.setState({active})
 
-    onGesture = e => {if (!this.state.active) Video.onGesture(e)}
+    onGesture = e => {
+        if (!this.state.active) Video.onGesture(e)
+    }
 
-    onKeydown = e => {if (!this.state.active) Video.onKeydown(e)}
+    onKeydown = e => {
+        if (this.state.active || e.key === 'NumpadEnter' || e.key === 'Enter')
+            this.tapmenu && this.tapmenu.onKeydown(e)
+        else
+            Video.onKeydown(e)
+    }
 
-    onMousedownCapture = e => {if (Video.testEvent(e)) e.preventDefault()}
+    onMousedownCapture = e => {
+        if (Video.testEvent(e)) e.preventDefault()
+    }
 
     render ({items, ...props}) {
         return (
@@ -21,6 +29,7 @@ export default class Menu extends mixinFocusable(Component) {
                 <TapMenu
                     actions={Object.entries(items).map(([icon, onClick]) => ({icon, onClick}))}
                     onActive={this.onActive}
+                    onRef={ref => {this.tapmenu = ref}}
                     {...props}
                 />
             </div>
