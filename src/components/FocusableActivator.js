684d5a25e4f1082490245e4d83e96be3a09f1519
Rob Huehn <rhuehn@gmail.com>
2017-06-09 21:53:40 -0700

    add FocusableActivator escape key close

--- src/components/FocusableActivator.js
+++ src/components/FocusableActivator.js
@@ -4,6 +4,10 @@ import {focus, mixinFocusable} from '@theatersoft/focus'
 export class FocusableActivator extends mixinFocusable(Component) {
     state = {active: false}
 
+    onKeydown = e => {
+        if (e.key === 'Escape') this.onClose()
+    }
+
     onClose = () => {
         if (this.state.active) {
             this.setState({active: false})
