4f456b7c3df145b0e93f00c646c01c67c7055084
Rob Huehn <rhuehn@gmail.com>
2017-06-12 08:48:40 -0700

    add FocusableSheet

---
+++ src/components/FocusableSheet.js
@@ -0,0 +1,33 @@
+import {h, Component} from 'preact'
+import {focus, mixinFocusable} from '@theatersoft/focus'
+import {Sheet} from '@theatersoft/components'
+
+export const FocusableSheet = () => Content => class extends mixinFocusable(Component) {
+    state = {active: false}
+
+    onKeydown = e => {
+        if (e.key === 'Escape') this.onClose()
+    }
+
+    onClose = () => {
+        if (this.state.active) {
+            this.setState({active: false})
+            setTimeout(() => focus.pop(), 250)
+        }
+    }
+
+    componentDidMount()
+    {
+        this.setState({active: true})
+        super.componentDidMount()
+    }
+
+    render ({...props}, {active}) {
+        console.log(this.props)
+        return (
+            <Sheet type="right" active={active} onClick={this.onClose}>
+                <Content {...props}/>
+            </Sheet>
+        )
+    }
+}
