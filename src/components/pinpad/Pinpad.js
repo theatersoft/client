8b12ddc2ee558b7747fc4ce25c73934671776c0a
Rob Huehn <rhuehn@gmail.com>
2017-06-14 15:08:37 -0700

    convert pinpad to Row/Col flex components

--- src/components/pinpad/Pinpad.js
+++ src/components/pinpad/Pinpad.js
@@ -1,5 +1,5 @@
 import {h, Component} from 'preact'
-import {Button} from '@theatersoft/components'
+import {Row, Col, Button} from '@theatersoft/components'
 import {focus, mixinFocusable} from '@theatersoft/focus'
 import rpc from '../../rpc'
 import {refresh} from '../../auth'
@@ -33,18 +33,12 @@ export default class Pinpad extends mixinFocusable(Component) {
         const key = n =>
             <Button floating primary label={String(n)} onClick={() => this.onkey(n)}/>
         return (
-            <div class={style.pinpad}>
-                <div class={style.text}>{pin}</div>
-                <div class={style.row}>
-                    {key(1)} {key(2)} {key(3)} <Button round accent icon="cross" onClick={this.onClear}/>
-                </div>
-                <div class={style.row}>
-                    {key(4)} {key(5)} {key(6)} {key(0)}
-                </div>
-                <div class={style.row}>
-                    {key(7)} {key(8)} {key(9)} <Button round accent icon="lock" onClick={this.onEnter}/>
-                </div>
-            </div>
+            <Col class={style.pinpad}>
+                <Row center class={style.text}>{pin}</Row>
+                <Row>{key(1)} {key(2)} {key(3)} <Button round accent icon="cross" onClick={this.onClear}/></Row>
+                <Row>{key(4)} {key(5)} {key(6)} {key(0)}</Row>
+                <Row>{key(7)} {key(8)} {key(9)} <Button round accent icon="lock" onClick={this.onEnter}/></Row>
+            </Col>
         )
     }
 }
