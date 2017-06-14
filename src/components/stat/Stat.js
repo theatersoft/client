36bea5c0b5d5feb1b3b1d6b665b70a6dc977a82d
Rob Huehn <rhuehn@gmail.com>
2017-06-14 13:35:02 -0700

    rm obsolete controls from stat, restyle

--- src/components/stat/Stat.js
+++ src/components/stat/Stat.js
@@ -1,49 +1,49 @@
 import {h, Component} from 'preact'
-import {Icon, Row, Col, row, rows, cols} from '@theatersoft/components'
+import {Button, classes} from '@theatersoft/components'
 import style from './stat.styl'
 import {connect} from '../../redux'
 
 const mapStateToProps = ({devices: {Hvac: {value} = {}}, Time}) => ({value, Time})
 
 export default connect(mapStateToProps)(class extends Component {
-    render ({value = {}, Time}, {active}) {
+    render ({value = {}, Time}) {
         const
             {OAT = 0, MODE = '', HUMID = '', Z1FAN = '', Z1RT = 0, Z1RH = 0, Z1HTSP = 0, Z1CLSP = 0} = value,
             _time = new Date(Time),
             date = _time.toLocaleDateString('en-US', {weekday: "short", month: "short", day: "numeric"}),
             time = _time.toLocaleTimeString('en-US', {hour: "numeric", minute: "numeric"}).toLowerCase(),
-            T = (t, a) => <div id={style[a]}>{t}</div>
+            T = (t, a) => <p class={style[a]}>{t}</p>
         return (
-            <section>
-                <Col class="col">
+            <div class={classes(style.stat, style.row)}>
+                <div class={style.col1}>
                     {T(date)}
                     {T(time, 'time')}
                     {T('OUTDOOR')}
                     {T(OAT + '°', 'ot')}
                     {T('STATUS')}
                     {T(MODE + ' / ' + Z1FAN)}
-                </Col>
-                <Col>
+                </div>
+                <div class={style.col2}>
                     {T(Z1RT + '°', 'rt')}
                     {T(`${Z1RH}% Humidity ${HUMID}`, 'hum')}
-                    <Row>
-                        <div class={style['col-1-2']}>
+                    <div class={style.row}>
+                        <div class={style.col}>
                             {T(Z1HTSP, 'htsp')}
-                            <Row>
-                                <div class={style['col-1-4']}><Icon icon="arrow-down"/></div>
-                                <div class={style['col-1-4']}><Icon icon="arrow-up"/></div>
-                            </Row>
+                            <div class={style.row}>
+                                <div class={style.col}><Button round inverse icon="arrow-down"/></div>
+                                <div class={style.col}><Button round inverse icon="arrow-up"/></div>
+                            </div>
                         </div>
-                        <div class={style['col-1-2']}>
+                        <div class={style.col}>
                             {T(Z1CLSP, 'clsp')}
-                            <Row>
-                                <div class={style['col-1-4']}><Icon icon="arrow-down"/></div>
-                                <div class={style['col-1-4']}><Icon icon="arrow-up"/></div>
-                            </Row>
+                            <div class={style.row}>
+                                <div class={style.col}><Button round inverse icon="arrow-down"/></div>
+                                <div class={style.col}><Button round inverse icon="arrow-up"/></div>
+                            </div>
                         </div>
-                    </Row>
-                </Col>
-            </section>
+                    </div>
+                </div>
+            </div>
         )
     }
 })
