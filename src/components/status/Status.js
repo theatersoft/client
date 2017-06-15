29fcf1f602dbd09086cc86e1b700ed9ba7635e90
Rob Huehn <rhuehn@gmail.com>
2017-06-15 07:53:38 -0700

    add date,time to status

--- src/components/status/Status.js
+++ src/components/status/Status.js
@@ -1,12 +1,21 @@
 import {h, Component} from 'preact'
 import {Row, Button} from '@theatersoft/components'
+import {connect} from '../../redux'
 
-export default class extends Component {
-    render () {
+const mapStateToProps = ({Time}) => ({Time})
+
+export default connect(mapStateToProps)(class extends Component {
+    render ({Time}) {
+        const
+            _time = new Date(Time),
+            date = _time.toLocaleDateString('en-US', {weekday: "short", month: "short", day: "numeric"}),
+            time = _time.toLocaleTimeString('en-US', {hour: "numeric", minute: "numeric"}).toLowerCase()
         return (
-            <Row>
+            <Row between>
+                <span>{time}</span>
+                <span>{date}</span>
                 <Button round inverse icon="spinner" onClick={() => window.location.reload()}/>
             </Row>
         )
     }
-}
+})
