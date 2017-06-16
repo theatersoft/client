910d54b2da9b58f9c51ae38de0388e0e9349c5db
Rob Huehn <rhuehn@gmail.com>
2017-06-15 21:08:44 -0700

    status last event experiment wip

--- src/components/status/Status.js
+++ src/components/status/Status.js
@@ -1,21 +1,36 @@
 import {h, Component} from 'preact'
-import {Row, Button} from '@theatersoft/components'
+import {List, ListItem, Switch, Row, Button} from '@theatersoft/components'
 import {connect} from '../../redux'
 
-const mapStateToProps = ({Time}) => ({Time})
+const
+    when = () => 'ago',
+    summary = ({name, status, id}) => (
+        <Row>
+            {name}, last {status} {when(id)}
+        </Row>
+    )
+
+const mapStateToProps = ({Time, devices}) => ({Time, devices})
 
 export default connect(mapStateToProps)(class extends Component {
-    render ({Time}) {
+    render ({Time, devices}) {
+        const {'Automation.feed': feed} = devices
+        console.log(feed)
         const
             _time = new Date(Time),
             date = _time.toLocaleDateString('en-US', {weekday: "short", month: "short", day: "numeric"}),
             time = _time.toLocaleTimeString('en-US', {hour: "numeric", minute: "numeric"}).toLowerCase()
         return (
-            <Row between>
-                <span>{time}</span>
-                <span>{date}</span>
-                <Button round inverse icon="spinner" onClick={() => window.location.reload()}/>
-            </Row>
+            <List>
+                <Row between>
+                    <span>{time}</span>
+                    <span>{date}</span>
+                    <Button round inverse icon="spinner" onClick={() => window.location.reload()}/>
+                </Row>
+                <ListItem label="Alarm armed"><Switch checked={false}/></ListItem>
+                <ListItem label="Away mode"><Switch checked={false}/></ListItem>
+                { feed && summary(feed.value)}
+            </List>
         )
     }
 })
