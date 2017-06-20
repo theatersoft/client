d19e058b15013d57cddf7e58c2c1b1903b46bfff
Rob Huehn <rhuehn@gmail.com>
2017-06-19 22:24:18 -0700

    add all devices to settings devices

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,15 +1,32 @@
 import {h, Component} from 'preact'
-import {List, ListItem, Switch, Row, Button} from '@theatersoft/components'
+import {List, NestedList, ListItem, Switch} from '@theatersoft/components'
 import {connect} from '../../redux'
+import {deviceAction, switchAction} from '../../actions'
 
-const mapStateToProps = ({devices, Time, offset}) => ({devices, Time, offset})
+const
+    mapStateToProps = ({devices = {}, Time, offset}) => ({
+        devices: Object.entries(devices)
+            //.filter(([k, v]) => ['Switch', 'Dimmer'].includes(v.type))
+            .map(([k, v]) => v),
+        Time,
+        offset
+    }),
+    mapDispatchToProps = dispatch => ({dispatchDeviceAction: action => dispatch(deviceAction(action))})
 
-export default connect(mapStateToProps)(class extends Component {
-    render ({Time, devices, offset}) {
+export default connect(mapStateToProps, mapDispatchToProps)(class extends Component {
+    render ({dispatchDeviceAction, Time, devices = [], offset}) {
         return (
             <List>
-                <ListItem label="Devices">
-                </ListItem>
+                <NestedList label="Devices">
+                    {devices.map(({name, id, value}) => {
+                        const click = () => dispatchDeviceAction(switchAction(value, id))
+                        return (
+                            <ListItem label={name} onClick={click}>
+                                <Switch checked={value} onChange={click}/>
+                            </ListItem>
+                        )
+                    })}
+                </NestedList>
             </List>
         )
     }
