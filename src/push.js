f0f18e1b0dd7d807f4d8b465419d90366410974f
Rob Huehn <rhuehn@gmail.com>
2017-06-22 22:54:15 -0700

    add applicationServerKey, register subscription with server

--- src/push.js
+++ src/push.js
@@ -1,11 +1,31 @@
+import rpc from './rpc'
+
 const log = (...args) => (console.log(...args), args[0])
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
 
 export const register = config => {
     navigator.serviceWorker.register('theatersoft-worker.js')
         .then(registration =>
             log(registration).pushManager.getSubscription()
                 .then(subscription =>
-                    log(subscription) || registration.pushManager.subscribe({userVisibleOnly: true})
+                    log(subscription) || registration.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: urlBase64ToUint8Array(config.publicKey)
+                    })
                 )
         )
         .then(subscription => {
@@ -15,11 +35,7 @@ export const register = config => {
             const authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : ''
             const endpoint = subscription.endpoint
 
-            fetch('./register', {
-                method: 'post',
-                headers: {'Content-type': 'application/json'},
-                body: JSON.stringify({endpoint, key, authSecret})
-            })
+            rpc('Session.Register', [{endpoint, key, authSecret}])
         })
 
 }
