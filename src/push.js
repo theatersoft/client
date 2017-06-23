accdd1375ed4145b5d99ac787a339fb07dba312e
Rob Huehn <rhuehn@gmail.com>
2017-06-23 07:53:11 -0700

    replace sample Uint8Array converter

--- src/push.js
+++ src/push.js
@@ -2,30 +2,17 @@ import rpc from './rpc'
 
 const log = (...args) => (console.log(...args), args[0])
 
-function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-}
+const Uint8ArrayOfUrlBase64 = b64 => new Uint8Array(atob(b64.replace(/_/g, '/').replace(/-/g, '+')).split('').map(c => c.charCodeAt(0)))
 
 export const register = config =>
     navigator.serviceWorker.register('theatersoft-worker.js')
         .then(registration =>
             log(registration).pushManager.getSubscription()
                 .then(subscription =>
-                    log(subscription) || registration.pushManager.subscribe({
+                    log(subscription) || registration.pushManager.subscribe(log({
                         userVisibleOnly: true,
-                        applicationServerKey: urlBase64ToUint8Array(config.publicKey)
-                    })
+                        applicationServerKey: Uint8ArrayOfUrlBase64(config.publicKey)
+                    }))
                 )
         )
         .then(subscription =>
--- src/worker/index.js
+++ src/worker/index.js
@@ -1,5 +1,5 @@
 self.addEventListener('push', event => {
-    const message = event.data ? event.data.json() : {body: 'no message'}
+    const message = event.data && event.data.json ? event.data.json() : {body: 'no message'}
 
     event.waitUntil(
         self.registration.showNotification('Theatersoft', {
