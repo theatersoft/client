1333c9223b249ab089770017e88c9eb8352fc398
Rob Huehn <rhuehn@gmail.com>
2017-06-23 09:36:41 -0700

    convert register rpc to bus method

--- src/push.js
+++ src/push.js
@@ -1,22 +1,18 @@
-import rpc from './rpc'
+import {bus, proxy} from '@theatersoft/bus'
 
 const log = (...args) => (console.log(...args), args[0])
 
-const Uint8ArrayOfUrlBase64 = b64 => new Uint8Array(atob(b64.replace(/_/g, '/').replace(/-/g, '+')).split('').map(c => c.charCodeAt(0)))
+const
+    Push = proxy('Push'),
+    Uint8ArrayOfUrlBase64 = b64 => new Uint8Array(atob(b64.replace(/_/g, '/').replace(/-/g, '+')).split('').map(c => c.charCodeAt(0)))
 
-export const register = config =>
-    navigator.serviceWorker.register('theatersoft-worker.js')
-        .then(registration =>
-            log(registration).pushManager.getSubscription()
-                .then(subscription =>
-                    log(subscription) || registration.pushManager.subscribe(log({
-                        userVisibleOnly: true,
-                        applicationServerKey: Uint8ArrayOfUrlBase64(config.publicKey)
-                    }))
-                )
+export const register = config => navigator.serviceWorker.register('theatersoft-worker.js')
+    .then(registration => log(registration).pushManager.getSubscription()
+        .then(subscription => log(subscription) || registration.pushManager.subscribe(log({
+                userVisibleOnly: true,
+                applicationServerKey: Uint8ArrayOfUrlBase64(config.publicKey)
+            }))
         )
-        .then(subscription =>
-            rpc('Session.Register', [subscription.toJSON()])
-                .catch(e => console.log(e))
-        )
+    )
+    .then(subscription => Push.register(log(document.cookie.slice(4)), subscription.toJSON()))
 
