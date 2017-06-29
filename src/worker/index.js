35a1e2c5272630cb3639de8399fd35783302a1cc
Rob Huehn <rhuehn@gmail.com>
2017-06-28 23:13:46 -0700

    focus client on notificationclick

--- src/worker/index.js
+++ src/worker/index.js
@@ -3,3 +3,12 @@ self.addEventListener('push', event => {
         self.registration.showNotification('Theatersoft', event.data.json())
     )
 })
+
+self.addEventListener('notificationclick', event => {
+    event.waitUntil(
+        self.clients.matchAll().then(clientList => {
+            if (clientList.length > 0) return clientList[0].focus()
+            return self.clients.openWindow('/')
+        })
+    )
+})
