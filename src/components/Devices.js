9531eb62d476a5e02e87d4d98c1317790e55fda7
Rob Huehn <rhuehn@gmail.com>
2017-07-17 12:28:08 -0700

    factor composable devices from settings

---
+++ src/components/Devices.js
@@ -0,0 +1,45 @@
+import {h, Component} from 'preact'
+import {List, NestedList, ListItem, Switch, Indicator} from '@theatersoft/components'
+import {connect} from '../redux'
+import {deviceAction} from '../actions'
+import {Type, Interface, interfaceOfType, switchActions} from '@theatersoft/device'
+
+const
+    isSwitch = type => interfaceOfType(type) === Interface.SWITCH_BINARY || interfaceOfType(type) === Interface.SWITCH_MULTILEVEL,
+    isIndicator = type => interfaceOfType(type) === Interface.SENSOR_BINARY,
+    switchable = type => isSwitch(type) && type !== Type.Siren
+
+const
+    mapStateToProps = ({devices = {}, Time, offset}) => ({devices, Time, offset}),
+    mapDispatchToProps = dispatch => ({dispatchDeviceAction: action => dispatch(deviceAction(action))})
+
+export const Devices = (ComposedComponent, props) => connect(mapStateToProps, mapDispatchToProps)(class extends Component {
+    onClick = e => {
+        const
+            id = e.currentTarget.dataset.id,
+            {value, type} = this.props.devices[id]
+        if (switchable(type)) this.props.dispatchDeviceAction(switchActions.toggle(value, id))
+    }
+
+    onChange = (value, e) => this.onClick(e)
+
+    render ({devices}) {
+        const
+            devicesByType = Object.values(devices).reduce((o, v) => (v.type && (o[v.type] || (o[v.type] = [])).push(v), o), {}),
+            deviceItem = ({name, id, value, type}) =>
+                <ListItem label={name} data-id={id} onClick={this.onClick}>
+                    {switchable(type) && <Switch checked={value} data-id={id} onChange={this.onChange}/>}
+                    {isIndicator(type) && <Indicator {...{normal: value === false, warning: value === true}} />}
+                </ListItem>,
+            typeItem = type =>
+                <NestedList label={type}>
+                    {devicesByType[type].map(deviceItem)}
+                </NestedList>
+        return (
+            <ComposedComponent {...props}>
+                {Object.keys(devicesByType).map(typeItem)}
+            </ComposedComponent>
+        )
+    }
+})
+
--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,49 +1,12 @@
 import {h, Component} from 'preact'
-import {List, NestedList, ListItem, Switch, Indicator} from '@theatersoft/components'
-import {connect} from '../../redux'
-import {deviceAction} from '../../actions'
-import {Type, Interface, interfaceOfType, switchActions} from '@theatersoft/device'
+import {List} from '@theatersoft/components'
+import {Devices} from '../'
 
-const
-    isSwitch = type => interfaceOfType(type) === Interface.SWITCH_BINARY || interfaceOfType(type) === Interface.SWITCH_MULTILEVEL,
-    isIndicator = type => interfaceOfType(type) === Interface.SENSOR_BINARY,
-    switchable = type => isSwitch(type) && type !== Type.Siren
-
-const
-    mapStateToProps = ({devices = {}, Time, offset}) => ({devices, Time, offset}),
-    mapDispatchToProps = dispatch => ({dispatchDeviceAction: action => dispatch(deviceAction(action))})
-
-export default connect(mapStateToProps, mapDispatchToProps)(class extends Component {
-    onClick = e => {
-        const
-            id = e.currentTarget.dataset.id,
-            {value, type} = this.props.devices[id]
-        if (switchable(type)) this.props.dispatchDeviceAction(switchActions.toggle(value, id))
-    }
-
-    onChange = (value, e) => this.onClick(e)
-
-    render ({devices}) {
-        const
-            devicesByType = Object.values(devices).reduce((o, v) => (v.type && (o[v.type] || (o[v.type] = [])).push(v), o), {}),
-            deviceItem = ({name, id, value, type}) =>
-                <ListItem label={name} data-id={id} onClick={this.onClick}>
-                    {switchable(type) && <Switch checked={value} data-id={id} onChange={this.onChange}/>}
-                    {isIndicator(type) && <Indicator {...{normal: value === false, warning: value === true}} />}
-                </ListItem>,
-            typeItem = type =>
-                <NestedList label={type}>
-                    {devicesByType[type].map(deviceItem)}
-                </NestedList>
-        return (
-            <List>
-                {Object.keys(devicesByType).map(typeItem)}
-            </List>
+export default class extends Component {
+    render () {
+        return h(
+            Devices(List) // TODO Devices(NestedList, {label: "Devices"})
         )
     }
-})
+}
 
-// TODO
-//<NestedList label="Devices">
-//    {Object.keys(devicesByType).map(typeItem)}
-//</NestedList>
