25bec81ce813de1fb3f24a694d0fc721977c7322
Rob Huehn <rhuehn@gmail.com>
2017-07-23 10:04:52 -0700

    add zwave service settings

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,21 +1,18 @@
 import {h, Component} from 'preact'
 import {List, NestedList, ListItem} from '@theatersoft/components'
 import {connect} from '../redux'
-
-import {ServiceSettings as AutomationServiceSettings} from '@theatersoft/automation'
-const settings = name => ({
-    Automation: AutomationServiceSettings
-}[name])
+import {ServiceSettings as Automation} from '@theatersoft/automation'
+import {ServiceSettings as ZWave} from '@theatersoft/zwave'
 
 const
-    mapStateToProps = ({config: {hosts}}) => ({hosts})
+    mapState = ({config: {hosts}}) => ({hosts})
 
-export const Services = (ComposedComponent, props) => connect(mapStateToProps)(class extends Component {
+export const Services = (ComposedComponent, props) => connect(mapState)(class extends Component {
     render ({hosts}) {
         const
             services = hosts.reduce((a, h) => (h.services && h.services.forEach(s => a.push(s)), a), []),
             serviceItem = ({name}) => {
-                const item = settings(name)
+                const item = {Automation, ZWave}[name]
                 return item ? h(item(NestedList, {label: name})) : <ListItem label={name}/>
             }
         return (
