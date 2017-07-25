71fd3ec899b52149c0cca9fabf087918404fa086
Rob Huehn <rhuehn@gmail.com>
2017-07-25 09:33:20 -0700

    add device settings as second indexed sheet

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -3,41 +3,60 @@ import {List, NestedList, ListItem, Switch, Indicator} from '@theatersoft/compon
 import {connect} from '../redux'
 import {deviceAction} from '../actions'
 import {Type, Interface, interfaceOfType, switchActions} from '@theatersoft/device'
+import {DeviceSettings as X10} from '@theatersoft/x10'
+import {DeviceSettings as ZWave} from '@theatersoft/zwave'
 
 const
     isSwitch = type => interfaceOfType(type) === Interface.SWITCH_BINARY || interfaceOfType(type) === Interface.SWITCH_MULTILEVEL,
     isIndicator = type => interfaceOfType(type) === Interface.SENSOR_BINARY,
-    switchable = type => isSwitch(type) && type !== Type.Siren
+    switchable = type => isSwitch(type) && type !== Type.Siren,
+
+    deviceSettings = id => {
+        const
+            [, service] = /^([^\.]+)\.([^]+)$/.exec(id) || [],
+            settings = {X10, ZWave}[service]
+        return settings || false
+    }
 
 const
     mapStateToProps = ({devices = {}, Time, offset}) => ({devices, Time, offset}),
     mapDispatchToProps = dispatch => ({dispatchDeviceAction: action => dispatch(deviceAction(action))})
 
 export const Devices = (ComposedComponent, props) => connect(mapStateToProps, mapDispatchToProps)(class extends Component {
+    state = {index: 0}
+
     onClick = e => {
+        const
+            id = e.currentTarget.dataset.id
+        if (deviceSettings(id)) this.setState({index: 1, id})
+    }
+
+    onSwitch = (_, e) => {
         const
             id = e.currentTarget.dataset.id,
             {value, type} = this.props.devices[id]
         if (switchable(type)) this.props.dispatchDeviceAction(switchActions.toggle(value, id))
     }
 
-    onChange = (value, e) => this.onClick(e)
-
-    render ({devices}) {
+    render ({devices}, {index, id}) {
         const
             devicesByType = Object.values(devices).reduce((o, v) => (v.type && (o[v.type] || (o[v.type] = [])).push(v), o), {}),
             deviceItem = ({name, id, value, type}) =>
                 <ListItem label={name} data-id={id} onClick={this.onClick}>
-                    {switchable(type) && <Switch checked={value} data-id={id} onChange={this.onChange}/>}
+                    {switchable(type) && <Switch checked={value} data-id={id} onChange={this.onSwitch}/>}
                     {isIndicator(type) && <Indicator {...{normal: value === false, warning: value === true}} />}
                 </ListItem>,
             typeItem = type =>
                 <NestedList label={type}>
                     {devicesByType[type].map(deviceItem)}
-                </NestedList>
+                </NestedList>,
+            settings = id && deviceSettings(id)
         return (
-            <ComposedComponent {...props}>
-                {Object.keys(devicesByType).map(typeItem)}
+            <ComposedComponent index={index}>
+                <subsection>
+                    {Object.keys(devicesByType).map(typeItem)}
+                </subsection>
+                {settings && h(settings('subsection'))}
             </ComposedComponent>
         )
     }
