72facd593219d3c92299d92062785ace194edb6a
Rob Huehn <rhuehn@gmail.com>
2017-08-12 21:01:51 -0700

    add base service settings wip

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,5 @@
 import {h, Component} from 'preact'
-import {List, NestedList, ListItem} from '@theatersoft/components'
+import {List, NestedList, ListItem, Subheader} from '@theatersoft/components'
 import {connect} from '../redux'
 import {ServiceSettings as Automation} from '@theatersoft/automation'
 import {ServiceSettings as ZWave} from '@theatersoft/zwave'
@@ -13,7 +13,7 @@ export const Services = (Composed, {label, next}) => connect(mapState)(class ext
     onClick = e => {
         const
             service = this.services[e.currentTarget.dataset.index],
-            settings = {Automation, ZWave}[service.export]
+            settings = {Automation, ZWave}[service.export] || ServiceSettings
         if (settings) next(props => h(settings('subsection', {service}), props))
     }
 
@@ -22,6 +22,16 @@ export const Services = (Composed, {label, next}) => connect(mapState)(class ext
             <Composed label={label}>
                 {this.services.map((service, index) =>
                     <ListItem label={service.name} data-index={index} onClick={this.onClick}/>)}
+            </Composed>
+        )
+    }
+})
+
+const ServiceSettings = (Composed, {service}) => connect(p => p)(class ServiceSettings extends Component {
+    render ({settings}) {
+        return (
+            <Composed>
+                <Subheader label={`${service.name} Service Settings`}/>
             </Composed>
         )
     }
