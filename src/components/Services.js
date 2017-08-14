b9996b9847bce33817f31e73a0dd9a1050dd0692
Rob Huehn <rhuehn@gmail.com>
2017-08-14 13:25:13 -0700

    add service specific settings in composed nested list

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,19 +5,18 @@ import {ServiceSettings as Automation} from '@theatersoft/automation'
 import {ServiceSettings as ZWave} from '@theatersoft/zwave'
 
 const
+    settingsMap = {Automation, ZWave},
     mapState = ({config: {hosts}}) => ({hosts})
 
 export const Services = (Composed, {label, next}) => connect(mapState)(class extends Component {
     services = this.props.hosts.reduce((a, h) => (h.services && h.services.forEach(s => a.push({host: h.name, ...s})), a), [])
 
     onClick = e => {
-        const
-            service = this.services[e.currentTarget.dataset.index],
-            settings = {Automation, ZWave}[service.export] || ServiceSettings
-        if (settings) next(props => h(settings('subsection', {service}), props))
+        const service = this.services[e.currentTarget.dataset.index]
+        next(props => h(ServiceSettings('subsection', {service}), props))
     }
 
-    render ({hosts}) {
+    render () {
         return (
             <Composed label={`${label} (${this.services.length})`}>
                 {this.services.map((service, index) =>
@@ -27,16 +26,19 @@ export const Services = (Composed, {label, next}) => connect(mapState)(class ext
     }
 })
 
-const ServiceSettings = (Composed, {service}) => connect(p => p)(class ServiceSettings extends Component {
-    render ({settings}) {
+const ServiceSettings = (Composed, {service, ...props}) => connect(p => p)(class ServiceSettings extends Component {
+    Settings = settingsMap[service.export] && settingsMap[service.export](NestedList, {service, ...this.props})
+
+    render () {
         const
             item = (label, value) => <ListItem label={label}><Switch checked={value}/></ListItem>,
             {name, enabled = true, host, module, export: _export, config} = service
         return (
             <Composed>
                 <Subheader label={`${name} Settings`}/>
+                {this.Settings && <this.Settings label={service.name} active/>}
                 {item('Enabled', enabled)}
-                <NestedList label="Service" active>
+                <NestedList label="more ...">
                     <Subheader label="Host"/>
                     <ListItem label={host}/>
                     <Subheader label="Module"/>
