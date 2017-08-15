79d02a8fc4a5464e43e12d4f087671df6b364a23
Rob Huehn <rhuehn@gmail.com>
2017-08-15 13:41:34 -0700

    tidy

--- src/store/index.js
+++ src/store/index.js
@@ -3,21 +3,14 @@ import thunk from 'redux-thunk'
 import {SET_CONFIG, SET_DEVICES, SET_TIME, SET_SETTINGS, SET_LOCALS, SET_NOTIFICATIONS} from '../actions'
 
 const
-    config = (state = false, {type, config}) => type === SET_CONFIG ? config : state,
-    devices = (state = {}, {type, devices}) => type === SET_DEVICES ? devices : state,
-    Time = (state = '', {type, Time}) => type === SET_TIME ? Time : state,
-    offset = (state = 0, {type, offset}) => type === SET_TIME ? offset : state,
-    settings = (state = {}, {type, settings}) => type === SET_SETTINGS ? settings : state,
-    locals = (state = {}, {type, locals}) => type === SET_LOCALS ? locals : state,
-    notifications = (state = {}, {type, notifications}) => type === SET_NOTIFICATIONS ? notifications : state,
     reducer = combineReducers({
-        config,
-        devices,
-        Time,
-        offset,
-        settings,
-        locals,
-        notifications
+        config: (state = false, {type, config}) => type === SET_CONFIG ? config : state,
+        devices: (state = {}, {type, devices}) => type === SET_DEVICES ? devices : state,
+        Time: (state = '', {type, Time}) => type === SET_TIME ? Time : state,
+        offset: (state = 0, {type, offset}) => type === SET_TIME ? offset : state,
+        settings: (state = {}, {type, settings}) => type === SET_SETTINGS ? settings : state,
+        locals: (state = {}, {type, locals}) => type === SET_LOCALS ? locals : state,
+        notifications: (state = {}, {type, notifications}) => type === SET_NOTIFICATIONS ? notifications : state,
     }),
     composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
