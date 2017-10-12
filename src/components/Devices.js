6a973fc41ba96c42e4d11a56c59517444a5e9220
Rob Huehn <rhuehn@gmail.com>
2017-10-12 14:01:45 -0700

    render dimmer device as slider

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -1,14 +1,14 @@
 import {h, Component} from 'preact'
-import {List, NestedList, ListItem, Switch, Indicator} from '@theatersoft/components'
+import {List, NestedList, ListItem, Switch, Slider, Indicator} from '@theatersoft/components'
 import {connect} from '../redux'
 import {deviceAction} from '../actions'
 import {Type, Interface, interfaceOfType, switchActions} from '@theatersoft/device'
 import {ComposeSheets, DeviceSettings} from './'
 
 const
-    isSwitch = type => interfaceOfType(type) === Interface.SWITCH_BINARY || interfaceOfType(type) === Interface.SWITCH_MULTILEVEL,
-    isIndicator = type => interfaceOfType(type) === Interface.SENSOR_BINARY,
-    switchable = type => isSwitch(type) && type !== Type.Siren
+    isSwitch = type => interfaceOfType(type) === Interface.SWITCH_BINARY && type !== Type.Siren,
+    isDimmer = type => interfaceOfType(type) === Interface.SWITCH_MULTILEVEL,
+    isIndicator = type => interfaceOfType(type) === Interface.SENSOR_BINARY
 
 const
     mapStateToProps = ({devices = {}, Time, offset}) => ({devices, Time, offset}),
@@ -24,17 +24,19 @@ export const DevicesSheet = (Composed, {label}) => ({next}) => connect(mapStateT
         const
             id = e.currentTarget.dataset.id,
             {value, type} = this.props.devices[id]
-        if (switchable(type)) this.props.dispatchDeviceAction(switchActions.toggle(value, id))
+        this.props.dispatchDeviceAction(switchActions.toggle(value, id))
     }
 
     render ({devices}, {index, id}) {
         const
             devicesByType = Object.values(devices).reduce((o, v) => (v.type && (o[v.type] || (o[v.type] = [])).push(v), o), {}),
             deviceItem = ({name, id, value, type}) =>
-                <ListItem label={name} data-id={id} onClick={this.onClick}>
-                    {switchable(type) && <Switch checked={value} data-id={id} onChange={this.onSwitch}/>}
-                    {isIndicator(type) && <Indicator {...{normal: value === false, warning: value === true}} />}
-                </ListItem>,
+                <ListItem label={name} data-id={id} onClick={this.onClick}>{
+                    isSwitch(type) ? <Switch checked={value} data-id={id} onChange={this.onSwitch}/>
+                        : isDimmer(type) ? <Slider value={value} data-id={id} onChange={this.onSwitch}/>
+                        : isIndicator(type) ? <Indicator {...{normal: value === false, warning: value === true}} />
+                        : null
+                }</ListItem>,
             typeItem = type =>
                 <NestedList label={type}>
                     {devicesByType[type].map(deviceItem)}
